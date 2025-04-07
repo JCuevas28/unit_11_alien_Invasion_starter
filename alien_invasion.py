953e21cf57adcd4e5b47a44d70c16804353a7ff4
Johnsy Cuevas <johnsycuevas@yahoo.com>
2025-04-06 20:52:30 -0400

    Added events for ship moving left and right, built in boundaries

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,47 +8,62 @@ class AlienInvasion:
     def __init__(self): 
         pygame.init()
         self.settings = Settings()
-       
         self.screen = pygame.display.set_mode(
             (self.settings.screen_w, self.settings.screen_h))
         
         pygame.display.set_caption(self.settings.name)  
-
         self.bg = pygame.image.load(self.settings.bg_file) 
         self.bg = pygame.transform.scale(self.bg, 
             (self.settings.screen_w, self.settings.screen_h)
-            )
-        
+        )
         self.running = True 
         self.clock = pygame.time.Clock()
 
         self.ship = Ship(self)
 
     def run_game(self):
-        # Game loop 
         while self.running:
-            self._check_events()                  
-            self._update_screen()                
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             self.clock.tick(self.settings.FPS)
 
-    
     def _check_events(self):
-        """Respond to keypresses and other events."""
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                self.running = False
-                pygame.quit()
-                sys.exit()
+                self._quit_game()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            self._quit_game()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
-        """Update images on the screen and flip to the new screen."""
         self.screen.blit(self.bg, (0, 0))
         self.ship.draw()
         pygame.display.flip()
+
+    def _quit_game(self):
+        self.running = False
+        pygame.quit()
+        sys.exit()
+
 
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
 
-    
     
--- ship.py
+++ ship.py
@@ -4,24 +4,36 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
 
-
-
-class Ship: 
-     
+class Ship:
     def __init__(self, game: 'AlienInvasion'):
         self.game = game
         self.settings = game.settings
-        self.screen = game.screen 
-        self.screen_rect = self.screen.get_rect()
+        self.screen = game.screen
+        self.boundaries = self.screen.get_rect()
 
         self.image = pygame.image.load(self.settings.ship_file)
         self.image = pygame.transform.scale(self.image,
             (self.settings.ship_w, self.settings.ship_h)
-            )
-        
-        
+        )
+
         self.rect = self.image.get_rect()
-        self.rect.midbottom = self.screen_rect.midbottom
+        self.rect.midbottom = self.boundaries.midbottom
+
+        
+        self.x = float(self.rect.x)
+
+        
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        temp_speed = self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.boundaries.right:
+            self.x += temp_speed
+        if self.moving_left and self.rect.left > self.boundaries.left:
+            self.x -= temp_speed
+
+        self.rect.x = self.x  
 
     def draw(self):
         self.screen.blit(self.image, self.rect)
