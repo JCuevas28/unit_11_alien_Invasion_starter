321f1502931a42b6bca12f3b1d5fd577df7b5ec1
Johnsy Cuevas <johnsycuevas@yahoo.com>
2025-04-06 23:56:27 -0400

    added bullets and limit to amound and laser sound

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,11 +2,13 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from arsenal import Arsenal
 
 class AlienInvasion:
     
     def __init__(self): 
         pygame.init()
+        pygame.mixer.init()  
         self.settings = Settings()
         self.screen = pygame.display.set_mode(
             (self.settings.screen_w, self.settings.screen_h))
@@ -19,7 +21,10 @@ class AlienInvasion:
         self.running = True 
         self.clock = pygame.time.Clock()
 
-        self.ship = Ship(self)
+        
+        self.laser_sound = pygame.mixer.Sound('Assets/sound/laser.mp3')  
+
+        self.ship = Ship(self, Arsenal(self))
 
     def run_game(self):
         while self.running:
@@ -42,14 +47,28 @@ class AlienInvasion:
             self.ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_SPACE:
+            if self.ship.fire():  
+                self.laser_sound.play()  
         elif event.key == pygame.K_q:
-            self._quit_game()
+            self.running = False
+            pygame.quit()
+            sys.exit()
 
     def _check_keyup_events(self, event):
+        
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key == pygame.K_LEFT:
             self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
 
     def _update_screen(self):
         self.screen.blit(self.bg, (0, 0))
@@ -65,5 +84,3 @@ class AlienInvasion:
 if __name__ == '__main__':
     ai = AlienInvasion()
     ai.run_game()
-
-    
--- ship.py
+++ ship.py
@@ -3,9 +3,11 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from alien_invasion import AlienInvasion
+    from arsenal import Arsenal
+
 
 class Ship:
-    def __init__(self, game: 'AlienInvasion'):
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
         self.game = game
         self.settings = game.settings
         self.screen = game.screen
@@ -13,27 +15,52 @@ class Ship:
 
         self.image = pygame.image.load(self.settings.ship_file)
         self.image = pygame.transform.scale(self.image,
-            (self.settings.ship_w, self.settings.ship_h)
-        )
+                                            (self.settings.ship_w, self.settings.ship_h))
 
         self.rect = self.image.get_rect()
         self.rect.midbottom = self.boundaries.midbottom
 
-        
         self.x = float(self.rect.x)
-
-        
+        self.y = float(self.rect.y)  # Add vertical position
+        self.arsenal = arsenal
         self.moving_right = False
         self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
 
     def update(self):
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+        # Update the ship's rect for horizontal movement
+        self.rect.x = self.x
+        self.rect.y = self.y  # Update vertical position
+
+    def _update_ship_movement(self):
+        
         temp_speed = self.settings.ship_speed
         if self.moving_right and self.rect.right < self.boundaries.right:
             self.x += temp_speed
-        if self.moving_left and self.rect.left > self.boundaries.left:
+        elif self.moving_left and self.rect.left > self.boundaries.left:
             self.x -= temp_speed
 
-        self.rect.x = self.x  
+        
+        if self.moving_up and self.rect.top > self.boundaries.top:
+            self.y -= temp_speed
+        elif self.moving_down and self.rect.bottom < self.boundaries.bottom:
+            self.y += temp_speed
+
+        
+        if self.rect.left <= self.boundaries.left:  
+            self.rect.centerx = self.boundaries.centerx
+            self.image = pygame.transform.flip(self.image, True, False)  
+        elif self.rect.right >= self.boundaries.right:  
+            self.rect.centerx = self.boundaries.centerx
+            self.image = pygame.transform.flip(self.image, True, False)  
 
     def draw(self):
+        self.arsenal.draw()
         self.screen.blit(self.image, self.rect)
+
+    def fire(self):
+        return self.arsenal.fire_bullet()
